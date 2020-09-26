a7ac6e6a3c4829137ee5fce33c0c1a7191acb7e8
treachGIT <65372846+treachGIT@users.noreply.github.com>
2020-09-26 14:35:30 +0200

    Options button and game pause added

---
+++ Assets/Code/UI_Scripts/PauseWhenClick.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseWhenClick : MonoBehaviour
+{
+    private bool gameIsPaused= false;
+
+    public void PauseGame()
+    {
+        if (gameIsPaused)
+        {
+            gameIsPaused = false;
+        }
+        else
+        {         
+            gameIsPaused = true;
+        }
+    }
+
+
+    void Update()
+    {
+       
+            if (!gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pauseee();
+            }
+        
+    }
+    void Resume()
+    {
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+
+    void Pauseee()
+    {
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+}
---
+++ Assets/pausew.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pausew : MonoBehaviour
+{
+    public static bool gameIsPaused;
+
+    void Update()
+    {
+        if (Input.GetKeyDown("m"))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pauseee();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+
+    void Pauseee()
+    {
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+}
